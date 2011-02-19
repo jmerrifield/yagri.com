a4af3bda2648704c2cc69aa8a26caf5abacd86f9
Jon Merrifield <jon@jmerrifield.com>
2011-02-19 12:46:08 +0000

    Fix for static files issue (from http://davidwparker.com/2009/11/13/sinatra-base-static-file-issue/)

--- lib/yagri/app.rb
+++ lib/yagri/app.rb
@@ -1,4 +1,8 @@
 class App < Sinatra::Base
+  # Need to set these for static files - http://davidwparker.com/2009/11/13/sinatra-base-static-file-issue/
+  set :static, true
+  set :public, 'public'
+
   get '/' do
     liquid :index
   end
