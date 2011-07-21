f53a9f414d4580dd95bd25335d0e4a961942b0ee
Jon Merrifield <jon@jmerrifield.com>
2011-07-21 08:19:51 +0100

    Gist tag now loads gist via HTTP GET to the JSON API, and renders it to the page along with the stylesheet import.

--- lib/liquid_filters/gist_tag.rb
+++ lib/liquid_filters/gist_tag.rb
@@ -1,3 +1,6 @@
+require 'rest-client'
+require 'json'
+
 module LiquidFilters
   class GistTag < Liquid::Tag
     def initialize(tag_name, text, token)
@@ -8,10 +11,24 @@ module LiquidFilters
     def render(context)
       if parts = @text.match(/([\d]*) (.*)/)
         gist, file = parts[1].strip, parts[2].strip
-        "<script src=\"https://gist.github.com/#{gist}.js?file=#{file}\"></script>"
+        get_raw_gist(gist, file) + get_styles_url
       else
         raise "Wrong parameter format"
       end
+    end
+
+    private
+    def get_json_url(gist, file)
+      "https://gist.github.com/#{gist}.json?file=#{file}"
+    end
+
+    def get_raw_gist(gist, file)
+      json = RestClient.get(get_json_url(gist, file))
+      JSON.load(json)['div']
+    end
+
+    def get_styles_url
+      "<link rel='stylesheet' href='https://gist.github.com/stylesheets/gist/embed.css'>"
     end
   end
 
