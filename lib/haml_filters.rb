b9af8d5487065ce7b2c5a63d409cb1044517822a
jon.merrifield <jon@jmerrifield.com>
2011-07-22 17:12:30 +0100

    Forgot to process Liquid tags in Atom feed

--- lib/haml_filters.rb
+++ lib/haml_filters.rb
@@ -1,19 +1,21 @@
 module Haml::Filters::EscapedMarkdown
-  include Haml::Filters::Base
-  require 'rdiscount'
+	include Haml::Filters::Base
+	require 'liquid'
+	require 'rdiscount'
 
-  def render(text)
-    Haml::Helpers.html_escape RDiscount.new(text).to_html
-  end
+	def render(text)
+		md = Liquid::Template.parse(text).render()
+		Haml::Helpers.html_escape RDiscount.new(md).to_html
+	end
 end
 
 module Haml::Filters::LiquidMarkdown
-  include Haml::Filters::Base
-  require 'liquid'
-  require 'rdiscount'
+	include Haml::Filters::Base
+	require 'liquid'
+	require 'rdiscount'
 
-  def render(text)
-    md = Liquid::Template.parse(text).render()
-    RDiscount.new(md).to_html
-  end
+	def render(text)
+		md = Liquid::Template.parse(text).render()
+		RDiscount.new(md).to_html
+	end
 end
