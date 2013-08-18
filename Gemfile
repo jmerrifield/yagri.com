dbdb879b5ca52c223b6bfff407761871ac5a79ad
Jon Merrifield <jon@jmerrifield.com>
2013-08-17 23:57:57 -0700

    Do a normal gist embed

--- Gemfile
+++ Gemfile
@@ -1,11 +1,9 @@
-source :rubygems
+source 'https://rubygems.org'
 gem 'sinatra', '1.1.2'
 gem 'haml', '3.0.25'
 gem 'rspec'
 gem 'rdiscount', '1.6.8'
 gem 'liquid'
-gem 'rest-client'
-gem 'json'
 gem "rack-force_domain", "~> 0.2.0"
 gem 'thin'
 
--- lib/liquid_filters/gist_tag.rb
+++ lib/liquid_filters/gist_tag.rb
@@ -1,6 +1,3 @@
-require 'rest-client'
-require 'json'
-
 module LiquidFilters
   class GistTag < Liquid::Tag
     def initialize(tag_name, text, token)
@@ -11,24 +8,10 @@ module LiquidFilters
     def render(context)
       if parts = @text.match(/([\d]*) (.*)/)
         gist, file = parts[1].strip, parts[2].strip
-        get_raw_gist(gist, file) + get_styles_url
+        "<script src='https://gist.github.com/#{gist}.js?file=#{file}'></script>"
       else
         raise "Wrong parameter format"
       end
-    end
-
-    private
-    def get_json_url(gist, file)
-      "https://gist.github.com/#{gist}.json?file=#{file}"
-    end
-
-    def get_raw_gist(gist, file)
-      json = RestClient.get(get_json_url(gist, file))
-      JSON.load(json)['div']
-    end
-
-    def get_styles_url
-      "<link rel='stylesheet' href='https://gist.github.com/stylesheets/gist/embed.css'>"
     end
   end
 
